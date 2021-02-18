8fb006eceb4e8121488e9766dde968a0eafb584b
talat595 <77441346+talat595@users.noreply.github.com>
2021-02-18 21:02:14 +0500

    chpter 1,2,3
    PIAIC chapter 1,2,3

--- chp 1/index.js
+++
@@ -1,7 +0,0 @@
-var firstName = "Talat"; 
-alert("firstName""+Talat");
-var lastName = "Perveen";
-alert("lastName""+Perveen");
-var age = 20;
-alert("age");
-console.log("hello"+"firstName"+"lastName"+"age");
---
+++ chpter 1,2,3/script.js
@@ -0,0 +1,7 @@
+var firstname = "Talat";
+var lastname = "Perveen";
+var age = 20;
+alert(firstname);
+alert(lastname);
+alert(age);
+alert("Hello Talat Perveen you are 20 years old");
