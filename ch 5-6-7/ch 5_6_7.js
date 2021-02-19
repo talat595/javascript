a39cf49e15388e22696346c3207c2d64af977ff3
talat595 <77441346+talat595@users.noreply.github.com>
2021-02-19 17:43:58 +0500

    update
    Java script assignment

--- ch 5-6-7/ch 5_6_7.js
+++
@@ -1,5 +0,0 @@
-var a = 2;
-var b;
- b = a++ + ++a - --a + a--;
-alert(a);
-alert(b);
---
+++ ch 5-6-7/prompt.js
@@ -0,0 +1,7 @@
+var firstname= prompt("enter firstname","");
+alert(firstname);
+var lastname = prompt("enter your lastname","");
+alert(lastname);
+var age= prompt("enter your age","");
+alert(age);
+alert("Hello"+" "+firstname+" "+lastname+" "+"you are"+age+" "+"years old");
