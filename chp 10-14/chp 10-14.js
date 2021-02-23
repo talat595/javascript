fecf0a49e0f79b469dff14c340d5824569f9a0e7
talat595 <77441346+talat595@users.noreply.github.com>
2021-02-23 23:02:17 +0500

    update
    chapter 10-14

--- chp 10-14/chp 10-14.js
+++
@@ -1,8 +0,0 @@
-var num1 = prompt("number");
-alert(num1);
-var num2 = prompt("number");
-alert(num2);
-var sign = prompt("multiplication");
-alert(sign);
-var result = prompt("7*3");
-alert(result);
---
+++ chp 10-14/chp10-14.js
@@ -0,0 +1,6 @@
+var num1=prompt("enter your number","");
+var num2= prompt("enter your number","");
+var sign= prompt("enter your sign","");
+var operation = 7+3;
+
+alert(operation);
